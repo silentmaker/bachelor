504c8bd6b3f3150628a785d6a51ad8995fdb0a54
马兴骢 <maxingcong@maxingcongdeMacBook-Pro.local>
2017-01-21 11:00:32 +0800

    init index.html

--- src/monkey/components.cljs
+++
@@ -1,7 +0,0 @@
-(ns monkey.components 
-    (:require [sablono.core :as sab]))
-
-(defn monkey-thumbs-up [data]
-    (sab/html [:div
-        [:h1 "how many thumbs up you want:" (:likes @data)]
-        [:div [:a {:href "#" :onClick #(swap! data update-in [:likes] inc)} "thumbs up"]]]))
