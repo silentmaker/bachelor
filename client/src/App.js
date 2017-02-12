84d3e58d7db557308b3da5a175bfa3987df2cbbc
马兴骢 <maxingcong@maxingcongdeMacBook-Pro.local>
2017-02-12 11:11:56 +0800

    default

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'whatwg-fetch';
+
 import './App.css';
+import logo from './images/logo.svg';
 
 class App extends Component {
+  componentDidMount() {
+    console.log('loaded');
+  }
+  handleClick() {
+    console.log('here');
+  }
   render() {
     return (
-      <div className="App">
+      <div className="App center">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div onClick={this.handleClick}>clicks me!</div>
       </div>
     );
   }
